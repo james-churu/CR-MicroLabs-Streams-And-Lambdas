8f32e0e16462c9a08661b0838f5f1e559a0d7111
jchuru <jameschuru123@gmail.com>
2020-03-22 16:01:14 -0400

    Anthropoid Finished

--- src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
+++ src/main/java/com/zipcodewilmington/streams/anthropoid/PersonFactory.java
@@ -3,6 +3,9 @@ package com.zipcodewilmington.streams.anthropoid;
 import com.zipcodewilmington.streams.tools.RandomUtils;
 import com.zipcodewilmington.streams.tools.StringUtils;
 
+import java.awt.event.PaintEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,7 +42,14 @@ public final class PersonFactory {
      * @return - ArrayList of Person objects
      */ // TODO
     public List<Person> createPersonList(int listSize) {
-        return null;
+
+        List<Person> people;
+        people = Stream
+                .generate(this::createRandomPerson)
+                .limit(listSize)
+                .collect(Collectors.toList());
+
+        return people;
     }
 
 
@@ -48,7 +58,15 @@ public final class PersonFactory {
      * @return - Array of Person objects
      */ // TODO
     public Person[] createPersonArray(int arrayLength) {
-        return null;
+
+        Person[] people;
+
+        people = Stream
+                .generate(this::createRandomPerson)
+                .limit(arrayLength)
+                .toArray(Person[] :: new);
+
+        return people;
     }
 
 
@@ -59,6 +77,8 @@ public final class PersonFactory {
      * @return - Stream representation of collection of Person objects
      */ // TODO
     public Stream<Person> createPersonStream(int streamCount) {
-        return null;
+
+
+        return Arrays.stream(createPersonArray(streamCount));
     }
 }
